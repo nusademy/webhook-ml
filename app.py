4ac81b8c2e17018b232b3d1f4ad98b3a3b029b95
muhnandajr <muhnandajr@gmail.com>
2021-05-27 14:55:57 +0700

    add prediction API to access model

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json
 import traceback
 import pandas as pd
 import numpy as np
+import pickle
 
 app = flask.Flask(__name__)
 
@@ -20,9 +21,34 @@ def webhook():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    return 'predict_test'
+    if model:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(model.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
 
 if __name__ == "__main__":
     app.secret_key = 'SecretKey'
+
+    with open("model.pkl", "rb") as f:
+        model = pickle.load(f)
+        print("model loaded into webhook")
+
+    with open("model_columns.pkl", "rb") as f:
+        model_columns = pickle.load(f)
+        print("model column loaded!")
+
     app.debug = True
     app.run()
